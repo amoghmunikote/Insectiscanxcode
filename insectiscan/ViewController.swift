0d1f133895b20ae8fe35f824629552bc512b6b0f
Jason Grife <jasongrife@hotmail.com>
2025-03-09 19:42:57 -0700

    Initial Commit

---
+++ insectiscan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  insectiscan
+//
+//  Created by Jason Grife on 3/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ insectiscanTests/insectiscanTests.swift
@@ -0,0 +1,17 @@
+//
+//  insectiscanTests.swift
+//  insectiscanTests
+//
+//  Created by Jason Grife on 3/9/25.
+//
+
+import Testing
+@testable import insectiscan
+
+struct insectiscanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
